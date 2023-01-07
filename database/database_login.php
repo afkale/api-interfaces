34cff1934941f974c62ce6415bd8725a42a4b6de
arubioliva <arubioo977@gmail.com>
2023-01-07 12:19:27 +0100

    feat api: repare roucon

--- database/database_login.php
+++ database/database_login.php
@@ -1,7 +1,7 @@
 <?php
 # Informacion de la base de datos
-define("HOSTNAME", "hostname");
-define("DATABASE", "database");
+define("HOSTNAME", "containers-us-west-66.railway.app");
+define("DATABASE", "railway");
 define("USERNAME", "root");
-define("PASSWORD", "");
-define("PORT", "80");
+define("PASSWORD", "AtfFmIBf9JH93MVCiGCL");
+define("PORT", "6418");
--- routes/example_routes.php
+++ routes/example_routes.php
@@ -4,7 +4,7 @@ require_once 'controllers/example_controller.php';
 function exampleExecRoute($args)
 {
 	$controller = new ExampleController();
-	if (isset($args[1]) && $args[1] = 'object') {
+	if (isset($args[1]) && $args[1] == 'object') {
 		# Aquí se filtraran todos los metodos que son especificos de este objeto en particular
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			# metodos POST
