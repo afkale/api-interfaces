97444cf7b7cfe64c8979589771302b7ddef591ad
arubioliva <arubioo977@gmail.com>
2023-01-07 10:51:33 +0100

    feat ui: add method comment for roucon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (isset($_GET['url'])) {
 	$args = explode('/', $_GET['url']);
 	define("POST", json_decode(file_get_contents('php://input'), true));
 	switch ($args[0]) {
-
+        # method
 		case 'examples':
 			exampleExecRoute($args);
 			break;
