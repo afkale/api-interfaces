9c0279795487eee2239dcc5d19d3b060f38bbe76
arubioliva <arubioo977@gmail.com>
2023-01-07 10:23:00 +0100

    feat api: remove useless file and add connection to gitignore

--- database/database_login.php
+++ database/database_login.php
@@ -1,7 +1,7 @@
 <?php
 # Informacion de la base de datos
-define("HOSTNAME", "containers-us-west-66.railway.app");
-define("DATABASE", "railway");
+define("HOSTNAME", "hostname");
+define("DATABASE", "database");
 define("USERNAME", "root");
-define("PASSWORD", "AtfFmIBf9JH93MVCiGCL");
-define("PORT", "6418");
+define("PASSWORD", "");
+define("PORT", "80");
--- simplehttpserver.py
+++
@@ -1,9 +0,0 @@
-from simple_http_server import route, server
-
-
-@route("/")
-def index():
-    return {"hello": "world"}
-
-
-server.start(port=9090)
