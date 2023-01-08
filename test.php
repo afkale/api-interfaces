e5b3c88b96c5658f17068a52a8cda6338bc4c739
arubioliva <arubioo977@gmail.com>
2023-01-08 15:40:21 +0100

    docs api: docs for testing

--- test.php
+++ test.php
@@ -7,12 +7,13 @@ if (isset($_GET['url'])) {
 	$args = explode('/', $_GET['url']);
 	define("POST", json_decode(file_get_contents('php://input'), true));
 	switch ($args[0]) {
-        # method
+		# method
 		case 'tests':
-			testExecRoute($args);
+			testExecRoute();
 			break;
+
 		case 'examples':
-			exampleExecRoute($args);
+			exampleExecRoute();
 			break;
 	}
 }
