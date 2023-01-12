a53d34897eedcad4571fe26f522b166127fe2148
arubioliva <arubioo977@gmail.com>
2023-01-12 19:23:15 +0100

    fix api: remove test table

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
 # path
-require_once './routes/test_routes.php';
 require_once './routes/example_routes.php';
 
 if (isset($_GET['url'])) {
 	$args = explode('/', $_GET['url']);
 	define("POST", json_decode(file_get_contents('php://input'), true));
 	switch ($args[0]) {
-        # method
-		case 'tests':
-			testExecRoute($args);
-			break;
+		# method
 		case 'examples':
 			exampleExecRoute($args);
 			break;
